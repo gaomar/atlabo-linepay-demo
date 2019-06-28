f14643a602aebe9a8f07698d655c8d05bc11c047
h-takauma <h-takauma@i-enter.co.jp>
2019-06-28 15:38:52 +0900

    初回コミット

--- src/router.js
+++ src/router.js
@@ -13,13 +13,17 @@ export default new Router({
       name: 'home',
       component: Home
     },
+/*    
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/pay/reserve',
+      name: 'reserve',
+      component: () => import('./views/Reserve.vue')
+    },
+    {
+      path: '/pay/confirm',
+      name: 'confirm',
+      component: () => import('./views/Confirm.vue')
     }
+*/
   ]
 })
