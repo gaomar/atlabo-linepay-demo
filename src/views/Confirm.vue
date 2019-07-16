94845645471f36e78db4df0205cebb5c704695e2
h-takauma <h-takauma@i-enter.co.jp>
2019-07-16 15:49:06 +0900

    LINE Things処理追加

--- src/views/Confirm.vue
+++ src/views/Confirm.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="confirm">
     <h1>{{status}}</h1>
+    <h2>{{bleStatus}}</h2>
   </div>
 </template>
 
@@ -8,11 +9,14 @@
 export default {
   data () {
     return {
-      status: ''
+      status: '',
+      USER_SERVICE_UUID: process.env.VUE_APP_USER_SERVICE_UUID,
+      LED_CHARACTERISTIC_UUID: 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B', /* トライアルは固定値 */
+      bleConnect: false,
+      bleStatus: 'デバイス未接続',
+      characteristic: '',
+      code: ''
     }
-  },
-  created () {
-    this.confirmAction()
   },
   methods: {
     confirmAction: function () {
@@ -35,9 +39,59 @@ export default {
         .then(response => {
           sessionStorage.clear()
           this.status = '決済完了しました！'
-          
+      
+          this.code = '1234'
+          const ch_array = this.code.split("");
+          for(let i = 0; i < 16; i = i + 1) {
+            ch_array[i] = (new TextEncoder('ascii')).encode(ch_array[i]);
+          }
+
+          this.characteristic.writeValue(new Uint8Array(ch_array)
+          ).catch(error => {
+            this.bleStatus = error.message
+          })
+
         })
+    },
+    // BLEが接続できる状態になるまでリトライ
+    liffCheckAvailablityAndDo: async function (callbackIfAvailable) {
+      try {
+        const isAvailable = await liff.bluetooth.getAvailability();
+        if (isAvailable) {
+          callbackIfAvailable()
+        } else {
+          // リトライ
+          this.bleStatus = `Bluetoothをオンにしてください。`
+          setTimeout(() => this.liffCheckAvailablityAndDo(callbackIfAvailable), 10000)
+        }
+      } catch (error) {
+        this.bleStatus = `Bluetoothをオンにしてください。`
+      }
+    },
+    // サービスとキャラクタリスティックにアクセス
+    liffRequestDevice: async function () {
+      const device = await liff.bluetooth.requestDevice()
+      await device.gatt.connect()
+      const service = await device.gatt.getPrimaryService(this.USER_SERVICE_UUID)
+      service.getCharacteristic(this.LED_CHARACTERISTIC_UUID).then(characteristic => {
+        this.characteristic = characteristic
+        this.bleConnect = true
+        this.bleStatus = `デバイスに接続しました！`
+        this.confirmAction()
+      }).catch(error => {
+        this.bleConnect = true
+        this.bleStatus = `デバイス接続に失敗=${error.message}`
+      })
+    },
+    initializeLiff: async function(){
+      await liff.initPlugins(['bluetooth']);
+      this.liffCheckAvailablityAndDo(() => this.liffRequestDevice())
     }
+  },
+  mounted: function () {
+    liff.init(
+        () => this.initializeLiff()
+    )
   }
 }
 </script>
